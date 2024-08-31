c79b03787c77b86eb55fa95404daece3fc694bfb
Hicks281 <mulengasilungwe123@gmail.com>
2024-08-31 19:19:26 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.getElementById('services').addEventListener('click', function() {
+    window.location.href = 'your-services-url.html';
+});
+
+document.getElementById('about-us').addEventListener('click', function() {
+    window.location.href = 'your-about-us-url.html';
+});
+
+document.getElementById('contact-us').addEventListener('click', function() {
+    window.location.href = 'your-contact-us-url.html';
+});
+
+// Add more event listeners for other buttons if necessary
